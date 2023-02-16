8f61583ba30e11259d1c365b05ff6135a9c5ec82
willl85 <ngowilfried@gmail.com>
2023-02-16 21:56:13 +0100

    use variables and the constructor

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -4,15 +4,46 @@ import logo from './logo.svg';
 import './App.css';
 
 /* Transformer un composant fonctionnel en composant de classe */
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+/* Utiliser les variables */
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Yves',
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p> Hi { this.state.name } </p>
+          <button>Change name</button>
           <a
             className="App-link"
             href="https://reactjs.org"
