c74c8ace3d930b898b4729d420408e386cb99cd4
willl85 <ngowilfried@gmail.com>
2023-02-17 16:31:32 +0100

    Using map() and optimze her to display a list

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,6 @@
 import { Component } from "react";
 
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 /* Transformer un composant fonctionnel en composant de classe */
@@ -70,52 +70,97 @@ import './App.css';
 // }
 
 /* Debugger la valeur de l'objet state avec une fonction callback */
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       name: { firstName: 'Yves', lastName: 'TEST' },
+//       company: 'Facebook',
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Hi {this.state.name.firstName} {this.state.name.lastName}, I work
+//             at {this.state.company}
+//           </p>
+//           <button
+//             onClick={() => {
+//               this.setState(
+//                 () => { 
+//                   return {
+//                     name: {firstName: 'Guy', lastName: 'BI'},
+//                   };
+                  
+//                 },
+//                 () => {
+//                   // Affiche la nouvelle valeur de this.state
+//                   console.log(this.state);
+//                 },
+//               );
+//             }} 
+//           >
+//             Change name
+//           </button>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+/* Using map() and optimze her to display a list  */
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      name: { firstName: 'Yves', lastName: 'TEST' },
-      company: 'Facebook',
-    }
+      monsters: [
+        {
+          id: '12e125e',
+          name: 'Dragon',
+        },
+        {
+          id: '12e125e210a',
+          name: 'Hydre',
+        },
+        {
+          id: '12e12',
+          name: 'Licorne',
+        },
+        {
+          id: '12e125e3',
+          name: 'Drago',
+        },
+      ],
+    };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName}, I work
-            at {this.state.company}
-          </p>
-          <button
-            onClick={() => {
-              this.setState(
-                () => { 
-                  return {
-                    name: {firstName: 'Guy', lastName: 'BI'},
-                  };
-                  
-                },
-                () => {
-                  // Affiche la nouvelle valeur de this.state
-                  console.log(this.state);
-                },
-              );
-            }} 
-          >
-            Change name
-          </button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          this.state.monsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+                <small>id: {monster.id}</small>
+              </div>
+            );
+          })
+        }
       </div>
     );
   }
